64950cf447b9b71b340e53c637b3f7a26dc8d2bd
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-07-04 11:36:23 +0200

    modif avec tiles

---
+++ assets/map/spritesheet.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="spritesheet" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../Nouveau dossier/spritesheet.png" width="96" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- game.js
+++ game.js
@@ -6,109 +6,74 @@ const config = {
     preload,
     create,
     update
-  }
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+      // Configurations spécifiques à Arcade Physics
+      // Par exemple, la gravité, la taille des collisions, etc.
+    }
+  }  
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('monstre', 'assets/monstre.png');
-  this.load.image('joueur', 'assets/joueur.png');
-  this.load.image('rocher', 'assets/rocher.png');
-  this.load.image('poteau', 'assets/poteau.png');
-  this.load.image('bombe', 'assets/bombe.png');
-  this.load.image('explosion', 'assets/explosion.png');
-  this.load.image('fond', 'assets/fond.png');
-  this.load.image('tile', 'assets/tile.png');
+  this.load.image('joueur', 'assets/joueur.png');   
+  this.load.image('fond', 'assets/fond.png');  
+  this.load.image("tiles", "./assets/map/spritesheet.png");
+  this.load.tilemapTiledJSON("map", "./assets/map/map.json");  
 }
 
 function create() {
   // Créer le fond de la carte
-  this.add.image(0, 0, 'fond').setOrigin(0);
-
-  // Créer un monstre
-  this.add.image(256, 128, 'monstre').setOrigin(0);
-
-  // Créer le joueur
-  this.player = this.add.image(64, 64, 'joueur').setOrigin(0); 
-
-  // Créer la bombe
-  this.add.image(320, 480, 'bombe').setOrigin(0);
-
+  this.add.image(0, 0, 'fond').setOrigin(0);  
+  
   // Récupérer les touches ZQSD
   this.keys = this.input.keyboard.addKeys('Z,Q,S,D');
-
-  // Création du contour de la carte
-  const tileSize = 32; // Taille d'une tuile
-  const mapWidth = Math.floor(this.game.config.width / tileSize);
-  const mapHeight = Math.floor(this.game.config.height / tileSize);
-
-  // Création des limites horizontales
-  for (let i = 0; i <= mapWidth; i++) {
-    this.add.image(i * tileSize, 0, 'tile').setOrigin(0);
-    this.add.image(i * tileSize, (mapHeight - 1) * tileSize, 'tile').setOrigin(0);
-  }
-
-  // Création des limites verticales
-  for (let i = 1; i < mapHeight - 1; i++) {
-    this.add.image(0, i * tileSize, 'tile').setOrigin(0);
-    this.add.image((mapWidth - 1) * tileSize, i * tileSize, 'tile').setOrigin(0);
-  }
-
-   // Créer les rochers
-  // Stocker les positions des rochers
-  this.rocherPositions = [];
-
-  // Générer une position aléatoire pour un rocher
-  const genererPositionAleatoire = function () {
-    const tileSize = 32; // Taille d'une tuile
   
-    // Générer une position aléatoire
-    let x = Phaser.Math.Between(tileSize, config.width - tileSize);
-    let y = Phaser.Math.Between(tileSize, config.height - tileSize);  
-
-    // Convertir la position en coordonnées de tuile
-    x = Phaser.Math.Snap.To(x, tileSize);
-    y = Phaser.Math.Snap.To(y, tileSize);
-
-    return { x, y };
-  };
-
-  // Générer les poteaux et rochers sur la carte
-  for (let x = tileSize; x < (mapWidth - 1) * tileSize; x += tileSize * 2) {
-    for (let y = tileSize; y < (mapHeight - 1) * tileSize; y += tileSize * 2) {
-      const poteau = this.add.image(x, y, 'poteau');
-      poteau.setOrigin(0, 0); // Définir l'origine en coin supérieur gauche du sprite
-
-      // Générer une position aléatoire pour un rocher
-      const position = genererPositionAleatoire.call(this);
-
-      // Ajouter la position à la liste des positions de rochers
-      this.rocherPositions.push(position);
-    }
-  }
-
-  for (let i = 0; i < 50; i++) {
-    const position = genererPositionAleatoire.call(this);
-    const rocher = this.add.image(position.x, position.y, 'rocher');
-    rocher.setOrigin(0, 0);
-  }
+  const map = this.make.tilemap({
+    key: "map",
+    tileWidth: 32,
+    tileHeight: 32,
+  });
+  
+  const tileset = map.addTilesetImage("spritesheet", "tiles");
+  const objet = map.createLayer("objet", tileset);
+  objet.setCollisionByProperty({ collides: true });
+  
+  // Créer le joueur
+  this.player = this.physics.add.sprite(32, 32, 'joueur').setOrigin(0);   
+  this.player.setCollideWorldBounds(true); // Définir les limites du monde pour le joueur
+  this.physics.add.collider(this.player, objet);
+  
+  
+  
 }
 
+
 function update() {
-  const speed = 5; // Vitesse de déplacement du joueur
+ 
 
   // Déplacement horizontal
   if (this.keys.Q.isDown) {
-    this.player.x -= speed;
+    this.player.setVelocityX(-200);
   } else if (this.keys.D.isDown) {
-    this.player.x += speed;
+    this.player.setVelocityX(200);
+  } else {
+    this.player.setVelocityX(0);
   }
-
+  
   // Déplacement vertical
   if (this.keys.Z.isDown) {
-    this.player.y -= speed;
+    this.player.setVelocityY(-200);
   } else if (this.keys.S.isDown) {
-    this.player.y += speed;
+    this.player.setVelocityY(200);
+  } else {
+    this.player.setVelocityY(0);
   }
+  
+  
 }
